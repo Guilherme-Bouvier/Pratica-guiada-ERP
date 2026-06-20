3cad5cf936e6a534997832e0085df219be072f5f
GUILHERME BOUVIER FONSECA RODRIGUES <guilhermebouva@gmail.com>
2026-06-19 21:41:44 -0300

    Aula Sexta

--- app/routes/web_db.py
+++ app/routes/web_db.py
@@ -33,3 +33,34 @@ def novo_produto_view():
                   return redirect(url_for("web.listar_produtos_view"))
             
       return render_template("produtos/form.html", produto=None, categorias=categorias)
+
+
+@web_db.route("/produto/excluir/<int:id>", methods=["DELETE"])
+def exlcuir_produto_view(id):
+      pass
+
+
+# ROTAS DE CATEEGORIA
+
+@web_db.route("/categorias")
+def listar_categorias_view():
+      categorias = categoria_controller.listar_todas_categorias()
+      return render_template("categorias/listar.html", categorias=categorias)
+
+@web_db.route("/categorias/novo", methods=["GET", "POST"])
+def nova_categoria_view():
+      if request.method == "POST":
+            nome = request.form.get("nome_categoria")
+
+            sucesso, msg = categoria_controller.salvar_categoria(nome)
+
+            flash(msg, "success" if sucesso else "danger")
+
+            if sucesso:
+                  return redirect(url_for('web.listar_categoria_view'))
+
+      return render_template("categorias/form.html", categoria=None)
+
+@web_db.route("/categoria/excluir/<int:id>", methods=["DELETE"])
+def exlcuir_categoria_view(id):
+      pass
